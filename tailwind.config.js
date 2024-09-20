d249052d75db28686cf3b322d1635e0860b34cf3
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-20 15:28:05 -0600

    configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      /* colors: {
+        charcoal: "#424f51",
+        teal: "#67aea7",
+        lightTeal: "#aae8ca",
+        beige: "#f0d9bc",
+        pink: "#e3315d",
+        black: "#060606",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"]
+      },
+      fontSize: {
+        para: '1.25rem',   // 20px
+        button: '1.5rem',  // 24px
+        title: '1.625rem', // 26px
+        heading: '3rem',   // 48px
+        hero: '4rem',      // 64px
+      }, */
+    },
   },
   plugins: [],
 }
