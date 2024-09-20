603e2ca90bb7254ccd70f43aae4b7c4266a6ffac
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-20 16:16:09 -0600

    create and display map with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import { APIProvider, Map } from '@vis.gl/react-google-maps'
 
+const apiKey = import.meta.env.VITE_MAPS_API_KEY;
+const mapId = import.meta.env.VITE_MAPS_ID;
+
 function App() {
 
+  const position = { lat: 46.869018, lng: -114.026812};
+
   return (
-    <>
+    <APIProvider apiKey={apiKey}>
+      <div style={{height: "100vh"}}>
+        <Map zoom={9} center={position} mapId={mapId}>
+        </Map>
+
+      </div>
       
-    </>
+    </APIProvider>
   )
 }
 
