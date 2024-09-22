d400eb7c8d586138a5199b3585ccb050f3a757bb
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-22 13:21:01 -0600

    set map default to USA

--- src/components/DisplayMap.jsx
+++ src/components/DisplayMap.jsx
@@ -5,13 +5,13 @@ const apiKey = import.meta.env.VITE_MAPS_API_KEY;
 const mapId = import.meta.env.VITE_MAPS_ID;
 
 export default function DisplayMap() {
+
+    const position = {lat: 41.4925 , lng: -99.9018};
     
-    const position = { lat: 46.869018, lng: -114.026812};
-  
     return (    
         <APIProvider apiKey={apiKey}>
             <div className='h-screen'>
-                <Map zoom={12} center={position} mapId={mapId}>
+                <Map zoom={4} center={position} mapId={mapId}>
                 </Map>
             </div>      
         </APIProvider>    
