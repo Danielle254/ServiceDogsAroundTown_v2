915b8518f5ce18c2110394c2e16b2fad4f8003e3
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-22 14:25:51 -0600

    center map to user location with button click

--- src/components/DisplayMap.jsx
+++ src/components/DisplayMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { APIProvider, Map } from '@vis.gl/react-google-maps'
 
 const apiKey = import.meta.env.VITE_MAPS_API_KEY;
@@ -6,12 +6,43 @@ const mapId = import.meta.env.VITE_MAPS_ID;
 
 export default function DisplayMap() {
 
-    const defaultCenter = {lat: 41.4925 , lng: -99.9018};    
+    const [position, setPosition] = useState({lat: 41.4925 , lng: -99.9018});
+    const [zoom, setZoom] = useState(4);
+    
+    function centerMapUserLocation() {
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, fail);
+        } else {
+            alert("Geolocation is not supported by your browser")
+        }
+    }
+
+    function success(pos) {
+        setPosition({lat: pos.coords.latitude, lng: pos.coords.longitude});
+        setZoom(12);
+    }
+
+    function fail() {
+        alert("Unable to retrieve your location");
+    }
     
     return (    
-        <APIProvider apiKey={apiKey}>
+        <APIProvider apiKey={apiKey}>            
             <div className='h-screen'>
-                <Map defaultZoom={4} defaultCenter={defaultCenter} mapId={mapId}>
+                <button onClick={centerMapUserLocation} className='absolute top-12 inset-x-1/2 w-32 bg-white z-10'>My City</button>
+                <Map 
+                zoom={zoom} 
+                center={position} 
+                mapId={mapId} 
+                reuseMaps={true} 
+                onCenterChanged={(map) => setPosition(map.detail.center)}
+                onZoomChanged={(map) => setZoom(map.detail.zoom)}
+                options={{
+                    gestureHandling: 'greedy',
+                    draggable: true
+                }}
+                >                    
                 </Map>
             </div>      
         </APIProvider>    
