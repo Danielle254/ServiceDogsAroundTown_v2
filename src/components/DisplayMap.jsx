fbaa6e5efe82c345c48bc1d873382c0fb9207649
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-22 13:46:11 -0600

    bug fix: unable to pan or zoom

--- src/components/DisplayMap.jsx
+++ src/components/DisplayMap.jsx
@@ -6,12 +6,12 @@ const mapId = import.meta.env.VITE_MAPS_ID;
 
 export default function DisplayMap() {
 
-    const position = {lat: 41.4925 , lng: -99.9018};
+    const defaultCenter = {lat: 41.4925 , lng: -99.9018};    
     
     return (    
         <APIProvider apiKey={apiKey}>
             <div className='h-screen'>
-                <Map zoom={4} center={position} mapId={mapId}>
+                <Map defaultZoom={4} defaultCenter={defaultCenter} mapId={mapId}>
                 </Map>
             </div>      
         </APIProvider>    
