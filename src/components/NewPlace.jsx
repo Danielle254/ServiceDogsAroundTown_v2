032cc8141585fd9b38004c7fea5aba43c4199f42
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-03 10:19:36 -0600

    add label and placeholder object state variable for new place

--- src/components/NewPlace.jsx
+++ src/components/NewPlace.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 export default function NewPlace(props) {
+    const [placeToAdd, setPlaceToAdd] = useState({});
+        // name, coords - from google maps
+        // date visited, safety issues (bool), safety issues description (text if yes)
+        // public note, denied access (bool), denied access description (text if yes)
     const ref = useRef();
+    function handleChange (e) {
+        let newValue = {}
+    }
 
     useEffect(() => {
         if (props.openNewPlace) {
@@ -10,20 +17,25 @@ export default function NewPlace(props) {
             ref.current?.close();
         }
     }, [props.openNewPlace]);
-    
+
+
+
+
     return (
         <dialog 
         ref={ref}
         onCancel={props.closeNewPlace}
+        className='w-96 h-96'
         >
+            <button onClick={props.closeNewPlace}>X</button>
             <h2>Add New Place</h2>
             <form>
-            <label htmlFor='date'></label>
-            <input
-            required
-            id='date'
-            type='date'
-            />
+                <label htmlFor='date'>Date Visited</label>
+                <input
+                required
+                id='date'
+                type='date'
+                />
             </form>
         </dialog>
         )
