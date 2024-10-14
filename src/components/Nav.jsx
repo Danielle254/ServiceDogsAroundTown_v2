ad5dddc5be3cb3a09c481f7a7c6fdfbf0aef8595
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-14 12:55:47 -0600

    remove login button - v3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,6 @@ export default function Nav(props) {
       <nav className='flex flex-row gap-4'>
         <button className={`border-2 border-gray-400 rounded p-2 bg-white ${props.page === 'map' ? 'border-blue-800' : ''}`} onClick={() => props.togglePage('map')}>Map</button>
         <button className={`border-2 border-gray-400 rounded p-2 bg-white ${props.page === 'about' ? 'border-blue-800' : ''}`} onClick={() => props.togglePage('about')}>About</button>
-        <button className='border-2 border-gray-400 rounded p-2 bg-white'>Login</button>
        </nav>
     </header>
     
