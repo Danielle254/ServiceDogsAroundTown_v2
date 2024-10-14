612586a6f3ef7dcf5d4918dc24672f3ba3d3e77c
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-14 13:13:41 -0600

    add content to footer and style to match figma design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      <p>footer text</p>
+    <div className='px-4 text-center'>
+      <p>Created with React, Tailwind CSS, Google Maps API, and Firebase.</p>
+      <a href='https://github.com/Danielle254/ServiceDogsAroundTown_v2' className='underline'>View Code</a>
     </div>
   )
 }
