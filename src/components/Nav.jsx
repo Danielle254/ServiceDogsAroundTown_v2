4a28716bffb524561dc622990321afe6a3b12383
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-14 13:11:29 -0600

    style: adjusted styling of nav buttons to reflect figma design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function Nav(props) {
 
+  const navButtonStyle = 'px-2 rounded-full'
+
   return (
-    <header className='flex flex-row justify-between pb-4 pt-4 px-4 bg-blue-300'>
+    <header className='flex flex-row justify-between pb-4 pt-4 px-4'>
       <h1 className='text-xl'>Service Dogs Around Town</h1>
       <nav className='flex flex-row gap-4'>
-        <button className={`border-2 border-gray-400 rounded p-2 bg-white ${props.page === 'map' ? 'border-blue-800' : ''}`} onClick={() => props.togglePage('map')}>Map</button>
-        <button className={`border-2 border-gray-400 rounded p-2 bg-white ${props.page === 'about' ? 'border-blue-800' : ''}`} onClick={() => props.togglePage('about')}>About</button>
+        <button className={`${navButtonStyle} ${props.page === 'map' ? 'bg-lightblue text-black' : ''}`} onClick={() => props.togglePage('map')}>Map</button>
+        <button className={`${navButtonStyle} ${props.page === 'about' ? 'bg-lightblue text-black' : ''}`} onClick={() => props.togglePage('about')}>About</button>
        </nav>
     </header>
     
