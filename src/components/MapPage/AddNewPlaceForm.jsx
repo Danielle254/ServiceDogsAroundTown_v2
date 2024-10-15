1eb04119b2ff69bc06336f053ec5d85c642d6904
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-15 08:44:07 -0600

    fix: change from a modal to a div that can be placed on the sidebar

--- src/components/MapPage/AddNewPlaceForm.jsx
+++ src/components/MapPage/AddNewPlaceForm.jsx
@@ -1,33 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 
-export default function NewPlace(props) {
+export default function NewPlace() {
+    
     const [placeToAdd, setPlaceToAdd] = useState({});
-        // name, coords - from google maps
-        // date visited, safety issues (bool), safety issues description (text if yes)
-        // public note, denied access (bool), denied access description (text if yes)
-    const ref = useRef();
-    function handleChange (e) {
-        let newValue = {}
-    }
-
-    useEffect(() => {
-        if (props.openNewPlace) {
-            ref.current?.showModal();
-        } else {
-            ref.current?.close();
-        }
-    }, [props.openNewPlace]);
-
-
-
 
     return (
-        <dialog 
-        ref={ref}
-        onCancel={props.closeNewPlace}
-        className='w-96 h-96'
-        >
-            <button onClick={props.closeNewPlace}>X</button>
+        <div className=''>
             <h2>Add New Place</h2>
             <form>
                 <label htmlFor='date'>Date Visited</label>
@@ -37,6 +15,6 @@ export default function NewPlace(props) {
                 type='date'
                 />
             </form>
-        </dialog>
+        </div>
         )
 }
