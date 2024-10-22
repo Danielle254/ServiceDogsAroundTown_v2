e690a035eeb8ad2ddd5b105b33f1b1ed2bd02409
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-22 11:33:07 -0600

    style: fix padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='px-4 text-center'>
-      <p>Created with React, Tailwind CSS, Google Maps API, and Firebase.</p>
-      <a href='https://github.com/Danielle254/ServiceDogsAroundTown_v2' className='underline'>View Code</a>
+    <div className='px-4 py-1 text-left text-sm'>
+      <p>Created with React, Tailwind CSS, Google Maps API, and Firebase. <span><a href='https://github.com/Danielle254/ServiceDogsAroundTown_v2' className='underline'>View Code</a></span></p>      
     </div>
   )
 }
