822d34ddd51f4a65ed62165979c6a6073345f49a
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-23 09:14:32 -0600

    style: create custom nav button classes for consistent use

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 export default function Nav({ page, togglePage }) {
 
-  const navButtonStyle = 'px-4 rounded-full font-bold'
-
   return (
     <header className='flex flex-row justify-between px-4 py-2'>
       <h1 className='text-xl'>Service Dogs Around Town</h1>
       <nav className='flex flex-row gap-4'>
-        <button className={`${navButtonStyle} ${page === 'map' ? 'bg-lightblue text-black' : ''}`} onClick={() => togglePage('map')}>Map</button>
-        <button className={`${navButtonStyle} ${page === 'about' ? 'bg-lightblue text-black' : ''}`} onClick={() => togglePage('about')}>About</button>
+        <button className={`btn-nav ${page === 'map' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('map')}>Map</button>
+        <button className={`btn-nav ${page === 'about' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('about')}>About</button>
        </nav>
     </header>
     
