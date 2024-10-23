cc2febecc7ebad6ca5b62f1b1f833fe5b766ee35
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-10-23 10:42:16 -0600

    feat: configure firebase to read and create new entries and store data in firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 import Footer from './components/Footer';
 import MapPage from './components/MapPage/MapPage';
 import AboutPage from './components/AboutPage/AboutPage'
+import {onSnapshot, addDoc } from 'firebase/firestore'
+import { entriesCollection} from './firebase.js'
 
 
 function App() {
   const [page, setPage] = useState('map');
-  const [places, setPlaces] = useState([
-    {coords: {lat: 46.75868, lng: -114.08766}, name: 'Lolo Steakhouse'}, 
-    {coords: {lat: 46.84997, lng: -114.01759}, name: "Paul's Pancake Parlour"}, 
-    {coords: {lat: 46.89410, lng: -114.04103}, name: 'test'}]);
+  const [places, setPlaces] = useState([]);
   const [addFormVisible, setAddFormVisible] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(entriesCollection, function(snapshot) {
+      const entriesArr = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }))
+      setPlaces(entriesArr);
+    })
+    return unsubscribe
+  }, []);
   
-    function togglePage (buttonId) {
-      if (buttonId === "map") {
-        setPage('map')
-      }
-      if (buttonId === 'about') {
-        setPage('about')
-      } 
+  function togglePage (buttonId) {
+    if (buttonId === "map") {
+      setPage('map')
     }
+    if (buttonId === 'about') {
+      setPage('about')
+    } 
+  }
 
-    function handleFormVisible() {
-      setAddFormVisible(true);
-    }
+  function handleFormVisible() {
+    setAddFormVisible(true);
+  }
 
-    function addNewPlace(e, place) {
-      e.preventDefault();
-      setPlaces([...places, place]);
-      setAddFormVisible(false);
-    }
+  async function addNewPlace(e, place) {
+    e.preventDefault();
+    const docRef = await addDoc(entriesCollection, place);
+    setAddFormVisible(false);
+  }
 
   return (
     <div className='h-screen flex flex-col bg-darkblue text-white'>
