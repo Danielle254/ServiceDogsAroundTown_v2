93c499e0b020629ecb6b355db3327d4faceca5d2
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-04 09:17:34 -0700

    style: change label-input arrangement from horizontal to vertical and adjust the font size

--- src/components/MapPage/AddNewPlaceForm.jsx
+++ src/components/MapPage/AddNewPlaceForm.jsx
@@ -78,9 +78,8 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
 
     return (
         <div className='px-2'>
-            <h2 className='text-lg text-center py-1'>Add New Place</h2>
-            <h3 className='px-1'>{name}</h3>
-            <h3 className='px-1'>{address}</h3>
+            <h3 className='mx-1 text-lg font-bold'>{name}</h3>
+            <h3 className='mx-1 mb-4'>{address}</h3>
             <form className='p-1' onSubmit={(e) => {
                 handleSubmit(e, newPlaceData);
                 setNewPlaceData({
@@ -96,22 +95,21 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                     rateFloor: 0,
                     publicNote: ''
                 });
-            }}>
-                <div className='flex flex-row justify-between'>
-                    <label htmlFor='visit-date'>Date Visited</label>
-                    <input
-                    required
-                    id='visit-date'
-                    type='date'
-                    value={newPlaceData.dateVisited}
-                    name='dateVisited'
-                    onChange={handleFormChange}
-                    className='text-black rounded'
-                    max={today}
-                    />
-                </div>
-                <fieldset>
-                    <legend>Were you Denied Access?</legend>
+            }}>                
+                <label htmlFor='visit-date' className='text-sm'>Date Visited</label>
+                <br/>
+                <input
+                required
+                id='visit-date'
+                type='date'
+                value={newPlaceData.dateVisited}
+                name='dateVisited'
+                onChange={handleFormChange}
+                className='text-black rounded'
+                max={today}
+                />                
+                <fieldset className='mt-4'>
+                    <legend className='text-sm'>Were you Denied Access?</legend>
                     <label htmlFor='access-issue-yes' className='pr-2'>Yes</label>
                     <input
                     type='radio'
@@ -135,7 +133,7 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                 </fieldset>
                 {newPlaceData.deniedAccess === 'true' && 
                 <>
-                <label htmlFor='access-issue-detail'>Please describe the issue and the outcome:</label>
+                <label htmlFor='access-issue-detail' className='text-sm mt-4'>Please describe the issue and the outcome:</label>
                 <br/>                
                 <textarea 
                 id='access-issue-detail'
@@ -145,8 +143,8 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                 onChange={handleFormChange}
                 maxLength={1000}
                 ></textarea></>}
-                <fieldset>
-                    <legend>Were there Safety Issues that affected your Service Dog?</legend>
+                <fieldset className='mt-4'>
+                    <legend className='text-sm'>Were there Safety Issues that affected your Service Dog?</legend>
                     <label htmlFor='safety-issue-yes' className='pr-2'>Yes</label>
                     <input
                     type='radio'
@@ -169,7 +167,7 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                     />
                 </fieldset>
                 {newPlaceData.safetyIssues === 'true' && <>
-                <label htmlFor='safety-issue-detail'>Please describe the safety issues:</label>
+                <label htmlFor='safety-issue-detail' className='text-sm mt-4'>Please describe the safety issues:</label>
                 <br/>
                 <textarea 
                 id='safety-issue-detail'
@@ -178,44 +176,41 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                 name='safetyIssuesDetails'
                 onChange={handleFormChange}
                 maxLength={1000}
-                ></textarea></>}
-                <div className='flex flex-row justify-between'>
-                    <label>Rate the Staff</label>
-                    <ReactStars
-                    count={5}
-                    size={24}
-                    isHalf={false}
-                    activeColor={'#24ACDD'}
-                    a11y={true}
-                    value={newPlaceData.rateStaff}
-                    onChange={updateStaffRating}
-                    />
-                </div>
-                <div className='flex flex-row justify-between'>
-                    <label title='was there sufficient room for your Service Dog to be out of the way of traffic?'>Rate the Space Ⓘ</label>
-                    <ReactStars
-                    count={5}
-                    size={24}
-                    isHalf={false}
-                    activeColor={'#24ACDD'}
-                    a11y={true}
-                    value={newPlaceData.rateSpace}
-                    onChange={updateSpaceRating}
-                    />
-                </div>
-                <div className='flex flex-row justify-between'>
-                    <label title='what was the condition of the floor?'>Rate the Floor Ⓘ</label>
-                    <ReactStars
-                    count={5}
-                    size={24}
-                    isHalf={false}
-                    activeColor={'#24ACDD'}
-                    a11y={true}
-                    value={newPlaceData.rateFloor}
-                    onChange={updateFloorRating}
-                    />
-                </div>
-                <label htmlFor='public-note'>Note for Other Visitors:</label>
+                ></textarea></>}                
+                <label className='text-sm'>Rate the Staff</label>
+                <br/>
+                <ReactStars
+                count={5}
+                size={24}
+                isHalf={false}
+                activeColor={'#24ACDD'}
+                a11y={true}
+                value={newPlaceData.rateStaff}
+                onChange={updateStaffRating}
+                />
+                <br/>
+                <label title='was there sufficient room for your Service Dog to be out of the way of traffic?' className='text-sm'>Rate the Space Ⓘ</label>
+                <ReactStars
+                count={5}
+                size={24}
+                isHalf={false}
+                activeColor={'#24ACDD'}
+                a11y={true}
+                value={newPlaceData.rateSpace}
+                onChange={updateSpaceRating}
+                />
+                <br/>
+                <label title='what was the condition of the floor?' className='text-sm mt-4'>Rate the Floor Ⓘ</label>
+                <ReactStars
+                count={5}
+                size={24}
+                isHalf={false}
+                activeColor={'#24ACDD'}
+                a11y={true}
+                value={newPlaceData.rateFloor}
+                onChange={updateFloorRating}
+                />                
+                <label htmlFor='public-note' className='text-sm'>Note for Other Visitors:</label>
                 <br/>
                 <textarea 
                 id='public-note'
@@ -228,7 +223,7 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                 ></textarea>
                 <input 
                 type='submit'
-                className='text-center bg-lightblue text-darkblue hover:bg-lightgreen font-bold rounded cursor-pointer w-full mb-2'
+                className='text-center bg-lightblue text-darkblue hover:bg-lightgreen font-bold rounded cursor-pointer w-full mt-4'
                 />
                 <button onClick={() => {
                     handleFormVisible();
@@ -245,7 +240,7 @@ export default function NewPlace({name, address, handleSubmit, handleFormVisible
                         rateFloor: 0,
                         publicNote: ''
                     });
-                }} className='text-center font-bold border-2 border-white rounded w-full hover:bg-white hover:text-darkblue'>Cancel</button>
+                }} className='text-center font-bold border-2 border-white rounded w-full hover:bg-white hover:text-darkblue mt-4'>Cancel</button>
             </form>
         </div>
         )
