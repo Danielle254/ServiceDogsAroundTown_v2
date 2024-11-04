1cfc89865f2551c43d8e285158f90c765ccc62b9
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-04 07:53:27 -0700

    fix: rearranged the navbar link order

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,11 +7,11 @@ export default function Nav({ page, togglePage }) {
     <header className='flex flex-row justify-between p-2'>
       <div className='inline-flex gap-2'>
         <img src={logo} className='h-8'/>
-        <h1 className='text-3xl font-title'>SERVICE DOGS AROUND TOWN</h1>
+        <h1 className='text-4xl font-title'>SERVICE DOGS AROUND TOWN</h1>
       </div>      
-      <nav className='flex flex-row gap-4'>
-        <button className={`btn-nav ${page === 'map' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('map')}>Map</button>
+      <nav className='flex flex-row gap-12 mr-4'>
         <button className={`btn-nav ${page === 'about' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('about')}>About</button>
+        <button className={`btn-nav ${page === 'map' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('map')}>Map</button>        
         <button className='btn-nav'>Login</button>
        </nav>
     </header>
