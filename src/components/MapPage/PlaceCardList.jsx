466bce64bbdd42ec8e692d75db0bc5dd84738a3a
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-06 12:22:28 -0700

    style: style place card

--- src/components/MapPage/PlaceCardList.jsx
+++ src/components/MapPage/PlaceCardList.jsx
@@ -4,7 +4,7 @@ import PlaceCard from './PlaceCard'
 export default function PlaceCardList({places}) {
 
     return(
-        <div className='mx-2 flex flex-col gap-3'>
+        <div className='mx-4 flex flex-col gap-3'>
             {places.map((place, index) =>                
                 <PlaceCard 
                 place={place}
