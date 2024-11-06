0c7ffbfe62119cbe290c85f11f4b653785ede3ec
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-06 12:51:51 -0700

    feat: create functionality for More/Less button to expand/collapse place details

--- src/components/MapPage/PlaceCard.jsx
+++ src/components/MapPage/PlaceCard.jsx
@@ -80,14 +80,17 @@ export default function PlaceCard({place}) {
         </div>
         }
       </div>
-      <p className='text-xs bg-white text-darkblue rounded my-1 px-1'>{publicNoteSummary(place.publicNote)}</p>
+      <p className='text-xs bg-white text-darkblue rounded my-1 px-1'>{showDetail ? place.publicNote : publicNoteSummary(place.publicNote)}</p>
       <div className='flex flex-row justify-between items-center my-1'>
         <p className='text-xs'>Last Visited: {formattedDate}</p>
         <div className='flex flex-row text-xs items-center cursor-pointer  pl-2 pr-1 text-lightblue rounded-full'>
-          <p>More</p>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          <p onClick={() => setShowDetail(!showDetail)}>{showDetail ? 'Less' : 'More'}</p>
+          {showDetail ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-5">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
           </svg>
+          : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-5">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>}
         </div>
       </div>
     </div>
