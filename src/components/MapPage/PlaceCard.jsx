b1577a06bc780e939cadbacf663ea1150303c5bf
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-06 12:45:04 -0700

    style: fix small styling issues with place card

--- src/components/MapPage/PlaceCard.jsx
+++ src/components/MapPage/PlaceCard.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactStars from 'react-rating-stars-component'
 
 
 export default function PlaceCard({place}) {
+  const [showDetail, setShowDetail] = useState(false);
   const visitYear = place.dateVisited.slice(0, 4);
   let visitMonth = place.dateVisited.slice(5, 7);
   if(visitMonth[0] === '0') {visitMonth = visitMonth[1]}  
@@ -21,15 +22,15 @@ export default function PlaceCard({place}) {
   }
 
   return (
-    <div className='bg-accentblue p-2 rounded shadow-sm shadow-gray-800'>
-      <p className='text-base font-bold mb-2'>{place.name}</p>    
+    <div className='bg-accentblue p-2 rounded shadow-sm shadow-gray-800'>         
       <div className='flex flex-row justify-between'>
         <div>
-          <p className='text-xs mb-2'>{streetAddress}</p>
+          <p className='text-base font-bold mb-1'>{place.name}</p> 
+          <p className='text-xs mb-1'>{streetAddress}</p>
           <p className='text-xs'>{cityState}</p>          
         </div>
         <div>
-          <div className='flex flex-row gap-2 justify-self-end items-center'>
+          <div className='flex flex-row gap-2 items-center min-w-[120px] justify-end'>
             <p className='text-xs'>Staff</p>
             <ReactStars
             count={5}
@@ -39,7 +40,7 @@ export default function PlaceCard({place}) {
             edit={false}
             />
           </div>
-          <div className='flex flex-row gap-2 justify-self-end items-center'>
+          <div className='flex flex-row gap-2 items-center min-w-[120px] justify-end'>
             <p className='text-xs'>Space</p>
             <ReactStars
             count={5}
@@ -49,7 +50,7 @@ export default function PlaceCard({place}) {
             edit={false}
             />
           </div>
-          <div className='flex flex-row gap-2 justify-self-end items-center'>
+          <div className='flex flex-row gap-2 items-center min-w-[120px] justify-end'>
             <p className='text-xs'>Floor</p>
             <ReactStars
             count={5}
@@ -63,7 +64,7 @@ export default function PlaceCard({place}) {
       </div>
       <div className='flex flex-row justify-between'>
         {place.deniedAccess === 'true' &&
-        <div className='text-xs py-1 mb-1 rounded-full w-2/5 flex flex-row  justify-center gap-2 items-center bg-red-900'>
+        <div className='text-xs py-1 my-1 rounded-full w-2/5 flex flex-row  justify-center gap-2 items-center bg-red-900'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
           <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
           </svg>
@@ -71,7 +72,7 @@ export default function PlaceCard({place}) {
         </div>
         }
         {place.safetyIssues === 'true' &&
-        <div className='text-xs py-1 mb-1 rounded-full w-2/5 bg-yellow-700 flex flex-row justify-center gap-2 items-center'>
+        <div className='text-xs py-1 my-1 rounded-full w-2/5 bg-yellow-700 flex flex-row justify-center gap-2 items-center'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-4">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
           </svg>
@@ -80,7 +81,7 @@ export default function PlaceCard({place}) {
         }
       </div>
       <p className='text-xs bg-white text-darkblue rounded my-1 px-1'>{publicNoteSummary(place.publicNote)}</p>
-      <div className='flex flex-row justify-between items-end my-1'>
+      <div className='flex flex-row justify-between items-center my-1'>
         <p className='text-xs'>Last Visited: {formattedDate}</p>
         <div className='flex flex-row text-xs items-center cursor-pointer  pl-2 pr-1 text-lightblue rounded-full'>
           <p>More</p>
