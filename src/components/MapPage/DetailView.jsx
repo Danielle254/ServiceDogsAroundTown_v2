10bb4e356089c1b8b9094a3c24446cd48af92018
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-11 10:39:15 -0700

    fix: allow user to close modal by clicking area outside of modal

--- src/components/MapPage/DetailView.jsx
+++ src/components/MapPage/DetailView.jsx
@@ -1,9 +1,15 @@
 import React, {forwardRef} from 'react'
 
-const DetailView = forwardRef((props, ref) => {
+const DetailView = forwardRef(({place, closeModal}, ref) => {
+    function closeFromEvent(event) {
+        if (event.target === event.currentTarget) {
+            closeModal();
+        }
+    }
     return (
-        <dialog ref={ref} className='w-1/2 h-1/2 bg-white text-black'>
-            {props.children}
+        <dialog ref={ref} className='w-1/2 h-1/2 bg-white text-black' onClick={closeFromEvent}>
+            <p>{place.name}</p>
+            <button onClick={closeModal}>Close</button>
         </dialog>
     );
 });
