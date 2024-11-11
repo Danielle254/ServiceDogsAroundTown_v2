eb5f06279f38400aa2a42cf940c4066324ce7388
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-11 12:31:59 -0700

    fix: ensure all expanded text areas are collapsed when modal is closed

--- src/components/MapPage/DetailView.jsx
+++ src/components/MapPage/DetailView.jsx
@@ -6,7 +6,6 @@ import formatDate from '../../utilities/formatDate'
 const DetailView = forwardRef(({place, closeModal}, ref) => {
     const [expandDeniedAccess, setExpandDeniedAccess] = useState(false);
     const [expandSafetyIssues, setExpandSafetyIssues] = useState(false);
-    const [expandpublicNote, setExpandPublicNote] = useState(false);
 
     function closeFromEvent(event) {
         if (event.target === event.currentTarget) {
@@ -15,7 +14,7 @@ const DetailView = forwardRef(({place, closeModal}, ref) => {
     }
     return (
         <dialog ref={ref} className='w-1/2 bg-white text-darkblue rounded shadow p-4 h-4/5 relative' onClick={closeFromEvent}>
-            <button onClick={closeModal} className='absolute top-4 right-4 hover:border-[1px] hover:border-darkblue'>
+            <button onClick={() => {closeModal(); setExpandDeniedAccess(false); setExpandSafetyIssues(false);}} className='absolute top-4 right-4 hover:border-[1px] hover:border-darkblue'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
