0cf785851f8d7cc5e2edeb17cf38313651801b08
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-12 15:17:59 -0700

    style: increase font sizes and spacing for better readability

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@ export default function Nav({ page, togglePage }) {
 
   return (
     <header className='flex flex-row justify-between p-2'>
-      <div className='inline-flex gap-2'>
+      <div className='flex flex-row items-center gap-2 ml-2'>
         <img src={logo} className='h-8'/>
-        <h1 className='text-4xl font-title'>SERVICE DOGS AROUND TOWN</h1>
+        <h1 className='text-title font-title'>SERVICE DOGS AROUND TOWN</h1>
       </div>      
-      <nav className='flex flex-row gap-12 mr-4'>
+      <nav className='flex flex-row gap-14 mr-4'>
         <button className={`btn-nav ${page === 'map' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('map')}>Home</button>
         <button className={`btn-nav ${page === 'about' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('about')}>About</button>                
         <button className='btn-nav flex flex-row items-center gap-1'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="size-5 ari">
