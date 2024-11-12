17df5926b6d3437d30490dc36cdb4973f6466249
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-12 14:56:01 -0700

    fix: reorder nav menu items and rename 'Map' to 'Home'

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,8 +10,8 @@ export default function Nav({ page, togglePage }) {
         <h1 className='text-4xl font-title'>SERVICE DOGS AROUND TOWN</h1>
       </div>      
       <nav className='flex flex-row gap-12 mr-4'>
-        <button className={`btn-nav ${page === 'about' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('about')}>About</button>
-        <button className={`btn-nav ${page === 'map' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('map')}>Map</button>        
+        <button className={`btn-nav ${page === 'map' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('map')}>Home</button>
+        <button className={`btn-nav ${page === 'about' ? 'btn-nav-selected' : ''}`} onClick={() => togglePage('about')}>About</button>                
         <button className='btn-nav'>Login</button>
        </nav>
     </header>
