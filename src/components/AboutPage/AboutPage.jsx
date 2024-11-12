1b8a16a5a706c091ffd1b5559709af1e3df6cf17
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-12 15:17:07 -0700

    style: increase spacing for better readability

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function AboutPage({ page }) {
   if (page === 'about') {
     return (
-      <div className='mx-4 mb-4 grid grid-cols-9 gap-8'>
-        <div className='col-span-4'>
+      <div className='mx-8 mb-8 grid grid-cols-9 gap-10'>
+        <div className='col-span-4 mt-8'>
           <img src='https://upload.wikimedia.org/wikipedia/commons/b/bf/Service_dogs_20181212-Library-52_%2846753606291%29.jpg' alt='yellow labrador retriever service dog wearing a green guide dogs for the blind vest looks up and to the right while laying down' className='w-full shadow-sm border-4 border-lightblue'/>
           <h3 className='text-xl mt-4 font-bold'>Easily save and reference information about local businesses as it affects the safety and comfort of your service dog.</h3>  
           <p className='text-sm mt-4'>Interested in the code? This web application was created with React, Tailwind CSS, Google Maps API, and Firebase. <span><a href='https://github.com/Danielle254/ServiceDogsAroundTown_v2' className='underline'>View Code</a></span></p>        
         </div>
         <div className='col-span-5'>
-          <h2 className='text-3xl font-bold mb-4'>Created for Service Dog Handlers by a Service Dog Handler</h2>
+          <h2 className='text-3xl font-bold m-8'>Created for Service Dog Handlers by a Service Dog Handler</h2>
           <p className='text-base'>This app was created to solve a personal problem - I couldn't always reliabily remember which restaurants I'd visited in town were good choices and which I should avoid going back to. What was the space like? How were the staff?</p>
           <br/>
           <p className='text-base'><span className='text-lightblue'>It's often difficult to get the information we need as service dog handlers to make good choices on where to visit and know what to expect.</span> I was researching Google photos and reviews, checking out the restaurant's website and social media, and sometimes even having to call to find out what I needed to plan my visit. Things like is all of your seating at bar height? My service dog needs easy access to me to alert and task; I can't be up on a barstool.</p>
