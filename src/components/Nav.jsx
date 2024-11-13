4060f76643182eb9257e608363449e51a3625359
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-13 09:59:30 -0700

    fix: add prop to NavLink to ensure homepage does not also render on other pages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,13 +13,13 @@ export default function Nav() {
       <nav>
         <ul className='flex flex-row gap-14 mr-4'>
           <li>
-            <NavLink to="/" className={({isActive}) => (isActive ? 'nav-link-active nav-link' : 'nav-link')}>Home</NavLink>
+            <NavLink to="/" className={({isActive}) => (isActive ? 'nav-link-active nav-link' : 'nav-link')} end>Home</NavLink>
           </li>
           <li>
-            <NavLink to="/about" className={({isActive}) => (isActive ? 'nav-link-active nav-link' : 'nav-link')}>About</NavLink>
+            <NavLink to="/about" className={({isActive}) => (isActive ? 'nav-link-active nav-link' : 'nav-link')} end>About</NavLink>
           </li>
           <li>
-            <NavLink to="/login" className={({isActive}) => (isActive ? 'nav-link-active nav-link flex flex-row gap-1 items-center' : 'nav-link flex flex-row gap-1 items-center')}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="size-5 ari">
+            <NavLink to="/login" className={({isActive}) => (isActive ? 'nav-link-active nav-link flex flex-row gap-1 items-center' : 'nav-link flex flex-row gap-1 items-center')} end><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="size-5 ari">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
             </svg>Login</NavLink>
           </li>
