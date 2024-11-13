9220a0f65666891e834fd88d9d2d0d067a93a871
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-13 09:21:49 -0700

    refactor: refactor code for react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
 import React from 'react'
+import { useOutletContext } from 'react-router-dom';
 import MapPage from './pages/MapPage';
 
 
-function App() {
-  const [places, setPlaces] = useState([]);
-  const [addFormVisible, setAddFormVisible] = useState(false);
-
-  useEffect(() => {
-    const unsubscribe = onSnapshot(entriesCollection, function(snapshot) {
-      const entriesArr = snapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-      }))
-      setPlaces(entriesArr);
-    })
-    return unsubscribe
-  }, []);
-
-  function handleFormVisible() {
-    setAddFormVisible(!addFormVisible);
-  }
-
-  async function addNewPlace(e, place) {
-    e.preventDefault();
-    const docRef = await addDoc(entriesCollection, place);
-    setAddFormVisible(false);
-  }
+function App() {  
+  const [places, addNewPlace, addFormVisible, handleFormVisible] = useOutletContext();
 
   return (
     <div className='h-screen flex flex-col bg-darkblue text-white'>
-      <Nav 
-      page={page}
-      togglePage={togglePage}
-      />
       <MapPage 
-      page={page}
       places={places}
       handleFormSubmit={addNewPlace}
       addFormVisible={addFormVisible}
       handleFormVisible={handleFormVisible}
-      />
-      <AboutPage
-      page={page}
       />
     </div>
   )
