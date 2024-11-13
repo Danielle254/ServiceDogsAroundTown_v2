30c6340a19dae819817588456720cc0f316787a3
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-13 09:01:36 -0700

    fix: create baseline code for react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Outlet, RouterProvider, createBrowserRouter} from 'react-router-dom';
+import App from './App.jsx';
+import About from './pages/About.jsx';
+import Login from './pages/Login.jsx';
+import Nav from './components/Nav.jsx';
+import ScrollToTop from './Components/ScrollToTop.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Layout = () => {
+  return (
+    <div>
+      <ScrollToTop />
+      <Nav />
+      <Outlet />
+    </div>
+  )
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
