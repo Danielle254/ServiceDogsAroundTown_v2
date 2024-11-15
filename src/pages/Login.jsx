73a9d2c768d98b492e3b2e73ac7a6cd966fbeec2
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-11-15 08:23:12 -0700

    feat: add user interface on login page for signing in with google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { auth } from '../firebase'
+import googleSignIn from '../assets/images/google sign in.png'
 
 export default function Login() {
   return (
-    <div>
-      <p>Login</p>
+    <div className='rounded shadow bg-white w-96 h-96 mx-auto my-auto flex flex-col justify-center items-center gap-10'>
+      <p className='text-lg text-darkblue text-center'>Login to create and manage your own entries</p>
+      <button className='w-4/5'><img src={googleSignIn} className='h-16'/></button>
+      
     </div>
   )
 }
