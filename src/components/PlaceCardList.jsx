cbf244f6385dcb97de8405d1bc9d5401de68616b
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-12-03 14:30:48 -0700

    feat: add conditional rendering depending on which type of list - all places or my places

--- src/components/PlaceCardList.jsx
+++ src/components/PlaceCardList.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import PlaceCard from './PlaceCard'
 
-export default function PlaceCardList({places, openModal}) {
+export default function PlaceCardList({places, openModal, type, userId}) {
 
-    return(
-        <div className='mx-4 flex flex-col gap-3'>
-            {places.map((place) =>                
-                <PlaceCard 
-                place={place}
-                key={place.id}
-                openModal={openModal}
-                />            
-            )}
-          </div>
-    )
+    if (type === 'allPlaces') {
+        return(
+            <div className='mx-4 flex flex-col gap-3'>
+                {places.map((place) =>                
+                    <PlaceCard 
+                    place={place}
+                    key={place.id}
+                    openModal={openModal}
+                    />            
+                )}
+              </div>
+        )
+    }
+
+    if (type === 'myPlaces') {
+        return(
+            <div className='mx-4 flex flex-col gap-3'>
+                {places.filter((place) => place.author === userId).map((place) =>                
+                    <PlaceCard 
+                    place={place}
+                    key={place.id}
+                    openModal={openModal}
+                    />            
+                )}
+              </div>
+        )
+    }
+
   
 }
